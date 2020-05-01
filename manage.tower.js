44c96cb639f4cff3c72bab7e15aed02913620cb5
methconi <methconi@onyx.re>
2020-05-01 10:16:40 +0200

    Tower forget target

--- manage.tower.js
+++ manage.tower.js
@@ -8,9 +8,10 @@ var manageTower = {
         if (target) { tower.attack(target); return; }
 
         var filter = builderRepairFilter();
+        tower.memory["target"] = null;
         target = findRepair(tower, filter);
         if(target) {
-            var res = tower.repair(target)
+            var res = tower.repair(target);
             if (res == OK) {
                 creepRecordEnergyUse(tower, "repair");
                 return;
