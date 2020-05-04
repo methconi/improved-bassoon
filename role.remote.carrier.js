dbc10a92700df8b83fada25b83a035f835a1e7ae
methconi <methconi@onyx.re>
2020-05-04 23:31:18 +0200

    Typo

--- role.remote.carrier.js
+++ role.remote.carrier.js
@@ -100,7 +100,7 @@ var roleRemoteCarrier = {
             } else {
                 creep.memory["mode"] = "store";
                 var target = null;
-                if (spawn.memory["energyToTerminal"]) [
+                if (spawn.memory["energyToTerminal"]) {
                     target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                         filter: structure => (structure.structureType == STRUCTURE_TERMINAL &&
                                               structure.store.getUsedCapacity(RESOURCE_ENERGY)
